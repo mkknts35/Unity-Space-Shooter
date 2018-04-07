b6e1eb657747e445f185088dbb2da26cab592fc0
Michael Koonts <mk.77@live.ca>
2018-04-07 15:28:10 -0700

    Initial commit at the end of the tutorial

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gc;
+
+    private void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if(gameControllerObject != null)
+        {
+            gc = gameControllerObject.GetComponent<GameController>();
+        }
+        if(gc == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Boundary" || other.tag == "Enemy")
+        {
+            return;
+        }
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+
+        if(other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gc.GameOver();
+        }
+        gc.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
